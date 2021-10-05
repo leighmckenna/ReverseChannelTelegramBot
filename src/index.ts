4b25594f68e1880aa5f4988fe381988661d76bf6
Leigh McKenna <leigh@leighmckenna.com>
2021-10-05 12:21:15 -0500

    basic new channel implimentation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
-import { Context, Telegraf } from 'telegraf'
+import { Context, Telegraf } from 'telegraf';
 import { AppUser, Channel } from './schema';
-import { token } from './config'
+import { token } from './config';
 
-const bot = new Telegraf(token)
+const bot = new Telegraf(token);
 
+//define global lists of users and channels
+var userList: AppUser[] = [];
+var chanList: Channel[] = [];
+
+//populate new user
+bot.start((ctx) => {
+    userList.push(<AppUser> {
+        UID: getUserUID(),
+        nameOnMsg: ctx.from.first_name,
+        tgUID: ctx.from.id,
+        chatOwned: [] as number[],
+        chatModed: [] as number[],
+        chatSender: [] as number[],
+        banned: false
+    })
+});
+
+//create a new channel
+bot.command('newchannel', (ctx) => {
+    chanList.push(
+        <Channel> {
+            UID: getChannelUID(),
+            owner: getUserFromTGID(ctx.from.id),
+            ops: [] as number[],
+            senders: [] as number[],
+            senderAlias: new Map<string, number>(),
+            joinLink: ctx.from.first_name
+        }
+    );
+
+});
 
 
 
@@ -11,5 +42,30 @@ const bot = new Telegraf(token)
 bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+////////////////////////////////////////////////////
+//
+//              utility functions
+//
+/// functions to support above methods and help 
+///        make things more readable 
+//
+////////////////////////////////////////////////////
+
+//currently makes UID for users, replace with actual UID generation later
+function getUserUID(): number {
+    return userList.length;
+}
+
+//currently makes UID for channels, replace with actual UID generation later
+function getChannelUID(): number {
+    return chanList.length;
+}
+
+function getUserFromTGID(telegramID: number): number {
+
+    return 0;
+}
+
--- src/schema.ts
+++ src/schema.ts
@@ -1,4 +1,8 @@
 
+enum Tasks {
+    Admin,
+    Message
+};
 
 interface AppUser {
     UID: number,
@@ -8,14 +12,20 @@ interface AppUser {
     chatModed: number[],
     chatSender: number[],
     banned: boolean
-}
+};
 
 interface Channel {
     UID: number,
     owner: number,
     ops: number[],
     senders: number[],
-    senderAlias: Map<string, number>
-}
+    senderAlias: Map<string, number>,
+    joinLink: string
+};
+
+interface UserStatus{
+    task: Tasks,
+    focusedChannel: number,
+};
 
 export {AppUser, Channel}
